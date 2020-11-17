1ec9f31900f295e4ca435cf7e6defe66dd09cd25
oluwablin <oyelekeseun@outlook.com>
2020-11-17 09:37:25 +0100

    creates registeration and authentication endpoints

--- routes/api.php
+++ routes/api.php
@@ -13,7 +13,22 @@ use Illuminate\Support\Facades\Route;
 | is assigned the "api" middleware group. Enjoy building your API!
 |
 */
+//NON-AUTHETICATED USERS
 
-Route::middleware('auth:api')->get('/user', function (Request $request) {
-    return $request->user();
+//REGISTER
+Route::post('user/register', 									        'RegistrationController@registerUser');
+
+//LOGIN
+Route::post('user/login', 										        'AuthenticationController@loginUser');
+
+//RESET
+Route::post('send/password/email',                                      'AuthenticationController@forgotPassword');
+Route::post('send/password/reset',                                      'AuthenticationController@resetPassword');
+
+
+//AUTHETICATED USERS
+Route::middleware('jwt.auth')->group(function () {
+    Route::get('get/authenticated/user', 							    'AuthenticationController@userCurrentlyLoggedIn');
+    Route::post('user/logout', 										    'AuthenticationController@logoutUser');
+
 });
